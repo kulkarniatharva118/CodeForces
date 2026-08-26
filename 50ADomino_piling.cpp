1aa9cdea0559b2fff77b83694d4c5fa0b7bd6d3f
Atharva Kulkarni <kulkarniatharva118@gmail.com>
2026-08-26 14:08:48 +0530

    Add Solution for 50A.Domino Piling

---
+++ 50ADomino_piling.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m,n=0;
+    int ans=0;
+    cin>>m>>n;
+    int area=m*n;
+
+    if(area%2==0){
+        ans=area/2;
+    }
+    else{
+        int rem=area%2;
+        area=area-rem;
+        ans=area/2;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
