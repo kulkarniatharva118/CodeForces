5d2a6ff4faa719b7c90e6bcaeea054996b01f38a
Atharva Kulkarni <kulkarniatharva118@gmail.com>
2026-08-27 13:49:39 +0530

    Add Solution for 236A.Boy or Girl

--- 236ABoy_or_Girl.cpp
+++ 236ABoy_or_Girl.cpp
@@ -8,6 +8,22 @@
 using namespace std;
 
 int main() {
-    
+    int freq[26]={};
+    string s;
+    int dis=0;
+    getline(cin >> ws,s);
+    for(char c:s){
+        freq[c-'a']++;
+        if(freq[c-'a']==1){
+            dis++;
+        }
+
+    }
+        if(dis%2==0){
+            cout<<"CHAT WITH HER!"<<endl;
+        }
+        else{
+            cout<<"IGNORE HIM!"<<endl;
+        }
     return 0;
 }
